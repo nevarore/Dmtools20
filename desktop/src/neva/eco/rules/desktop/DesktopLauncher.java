b90b250524449d53f99b84cecee8acf4d5e0539f
vdr <romu.vdr@gmail.com>
2014-11-03 10:09:40 +0100

    1.0.0

---
+++ desktop/src/neva/eco/rules/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package neva.eco.rules.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import neva.eco.rules.DmTools;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DmTools(), config);
+	}
+}
---
+++ html/src/neva/eco/rules/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package neva.eco.rules.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import neva.eco.rules.DmTools;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new DmTools();
+        }
+}
